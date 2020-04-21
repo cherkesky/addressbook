8bc5fc708239247b29c9f052e5d8cb6b22acf5b2
Cherkesky <guy@cherkesky.com>
2020-04-21 18:45:01 -0500

    added a contact class

---
+++ Contact.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace addressbook{
+  public class Contact{
+    public string FirstName  {get; set;}
+    public string LastName  {get; set;}
+    public string Email  {get; set;}
+    public string Address  {get; set;}
+
+    public string FullName {
+      get{
+      return $"FirstName+LastName";
+      }
+    }
+    public Contact (string first, string last, string email, string address){
+      FirstName = first;
+      LastName = last;
+      Email=email;
+      Address=address;
+    }
+
+  }
+}
